809ce6493a4f5aa2d0f66f2f67b4242a7e2da089
ziatdinovmax <ziatdinovmax@gmail.com>
2019-03-13 01:33:12 -0400

    corrected filename (exp data file)

--- DefectNet/packages.sh
+++ DefectNet/packages.sh
@@ -3,7 +3,7 @@ echo "\nCopying files from DefectNet directory..."
 cp -r AICrystallographer/DefectNet/* . 
 echo "\nDownloading experimental data...\n"
 gdown https://drive.google.com/uc?id=1Zz9iSr5eUsV9wHUzebbdp_Szr6GUiibf
-mv 'WS2stack_subset.npy' exp_data/
+mv 'WS2stack4_subset.npy' exp_data/
 echo "\nDownloading and installing PyTorch..."
 pip install https://download.pytorch.org/whl/cu90/torch-1.0.0-cp36-cp36m-linux_x86_64.whl > /dev/null
 echo "Checking if PyTorch sees Colab GPU device..."
