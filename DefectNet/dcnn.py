4b5fadb059854fbfd3c94640c12d14a0d55ea721
ziatdinovmax <ziatdinovmax@gmail.com>
2019-03-17 22:03:22 -0400

    Made number of resblocks optional (DefectNet)

--- DefectNet/dcnn.py
+++ DefectNet/dcnn.py
@@ -55,7 +55,7 @@ class ResSniffer(nn.Module):
     """
     Convolutional neural network with residual blocks
     """
-    def __init__(self, nb_filters=16, lrelu_a=0, with_logits=False):
+    def __init__(self, input_channels=32, res_depth=4, lrelu_a=0, with_logits=False):
         """
         Args:
             nb_filters (int): number of filters in the 1st layer
@@ -65,24 +65,33 @@ class ResSniffer(nn.Module):
             activation
         """
         super(ResSniffer, self).__init__()
+        output_channels = input_channels*2
         self.with_logits = with_logits
+        # Define first layer
         self.conv_in = nn.Sequential(
-            nn.Conv2d(1, nb_filters, kernel_size=3, stride=1, padding=1),
-            nn.LeakyReLU(negative_slope=lrelu_a, inplace=True))
+            nn.Conv2d(1, input_channels, kernel_size=3, stride=1, padding=1),
+            nn.LeakyReLU(negative_slope=lrelu_a, inplace=True),
+            nn.BatchNorm2d(input_channels))
+        # Define pre-last layer
         self.conv_out = nn.Sequential(
-            nn.Conv2d(nb_filters*2, nb_filters, kernel_size=3, stride=1, padding=1),
-            nn.LeakyReLU(negative_slope=lrelu_a, inplace=True))
-        self.conv_px = nn.Conv2d(nb_filters, 1, kernel_size=1, stride=1,
-                                 padding=0)
-        self.res_block1 = ResBlock(nb_filters, nb_filters*2, lrelu_a=lrelu_a)
-        self.res_block2 = ResBlock(nb_filters*2, nb_filters*2, lrelu_a=lrelu_a)
+            nn.Conv2d(output_channels, input_channels, kernel_size=3, stride=1, padding=1),
+            nn.LeakyReLU(negative_slope=lrelu_a, inplace=True),
+            nn.BatchNorm2d(input_channels))
+        # Define final layer (1-by-1 convolution)
+        self.conv_px = nn.Conv2d(input_channels, 1, kernel_size=1, stride=1, padding=0)
+        # Define residual block
+        res_module = []
+        for i in range(res_depth):
+            input_channels = output_channels if i > 0 else input_channels
+            res_module.append(
+                ResBlock(input_channels, output_channels, lrelu_a=lrelu_a))
+        self.res_module = nn.Sequential(*res_module)
 
     def forward(self, x):
         """Defines a forward path """
         x = self.conv_in(x)
         x = F.max_pool2d(x, kernel_size=2, stride=2)
-        x = self.res_block1(x)
-        x = self.res_block2(x)
+        x = self.res_module(x)
         x = F.interpolate(
             x, scale_factor=2, mode='bilinear', align_corners=False)
         x = self.conv_out(x)
