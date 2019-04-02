5dab497b585601f4644fce5d3beda0977089f9e5
Maxim Ziatdinov <34245227+ziatdinovmax@users.noreply.github.com>
2019-04-01 23:44:12 -0400

    Update packages.sh script for DefectNet

--- DefectNet/packages.sh
+++ DefectNet/packages.sh
@@ -1,13 +1,37 @@
 #!/bin/bash
+
+colabdevice() {
+python - <<END
+import torch
+device_count = torch.cuda.device_count()
+if device_count > 0:
+    print('Available GPU devices:')
+    for i in range(device_count):
+        print(torch.cuda.get_device_name(i))
+else:
+    print('No GPU device detected.',\
+    'If you would like to train a neural network,', \
+    'please select GPU as a hardware accelerator', \
+    'in Runtime --> Change runtime type',\
+    'or in Edit --> Notebook settings')
+END
+}
+
 echo "\nCopying files from DefectNet directory..."
 cp -r AICrystallographer/DefectNet/* . 
 echo "\nDownloading experimental data...\n"
 gdown https://drive.google.com/uc?id=1Zz9iSr5eUsV9wHUzebbdp_Szr6GUiibf
 mv 'WS2stack4_subset.npy' exp_data/
-echo "\nDownloading and installing PyTorch..."
-pip install https://download.pytorch.org/whl/cu90/torch-1.0.0-cp36-cp36m-linux_x86_64.whl > /dev/null
-echo "Checking if PyTorch sees Colab GPU device..."
-python AICrystallographer/FerroNet/colabdevice.py
+python -c "import torch" 2>/dev/null
+if test $? -ne 0; then
+    echo "\nDownloading and installing PyTorch -->\n"
+    pip install https://download.pytorch.org/whl/cu90/torch-1.0.0-cp36-cp36m-linux_x86_64.whl    
+else
+    echo "\nFound existing installation of PyTorch"
+    pip show torch | grep Version
+fi
+echo "\nChecking if PyTorch sees Colab GPU device..."
+colabdevice
 echo "\nInstalling packages for vizualization of neural networks..."
 pip install pydot && apt-get install graphviz > /dev/null
 pip install git+https://github.com/szagoruyko/pytorchviz > /dev/null
