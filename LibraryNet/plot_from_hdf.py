5ae35fc7abf0787681f4653490771f35bba49596
ziatdinovmax <ziatdinovmax@gmail.com>
2019-04-21 09:14:06 +0000

    Can now read both raw and processed (library) data

--- LibraryNet/plot_from_hdf.py
+++ LibraryNet/plot_from_hdf.py
@@ -2,6 +2,12 @@ import sys
 import matplotlib.pyplot as plt
 from utils import *
 
-imgdata, _ = open_hdf(sys.argv[1])
-plt.imshow(imgdata, cmap='gray')
-plt.show()
+try:
+    imgdata, _ = open_hdf(sys.argv[1])
+    plt.imshow(imgdata, cmap='gray')
+    plt.show()
+except KeyError:
+    imgdata, _, coord = open_library_hdf(sys.argv[1], atom_bond_dict()[0])
+    plt.imshow(imgdata[0, :, :, 0], cmap='gray')
+    plt.scatter(coord[:, 1], coord[:, 0], c=coord[:, 2], s=30, cmap='RdYlGn')
+    plt.show()
